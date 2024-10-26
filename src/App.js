7d14ff3cc83a9dd3e79aefd6800ff6f64a7989fd
H. Schnelle <41087994+hssw24@users.noreply.github.com>
2024-10-26 12:35:10 +0200

    Update App.js x6

--- src/App.js
+++ src/App.js
@@ -35,9 +35,9 @@ const VolumeMonitor = () => {
             const decibelsX = decibels;
             const decibels = 20 * Math.log10(avgVolume + 1); // Dezibelberechnung
             setDisplayedVolume((prev) => (prev * 0.8 + decibels * 0.2).toFixed(2)); // Glättung der Anzeige
-//alert ausgabe;
-  //          ausgabe = ausgabe + " Zusatz - "  ;
-//alert ausgabe;
+alert(ausgabe);
+            ausgabe = ausgabe + " Zusatz - "  ;
+alert(ausgabe);
             // Wenn Lautstärke höher als Grenzwert und Alarm noch nicht aktiv, Alarm auslösen
             if (decibels > threshold && !alarmActive) {
                 triggerAlarm();
