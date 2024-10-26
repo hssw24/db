607e8a0146da427db123ba14538a7056b531db3f
H. Schnelle <41087994+hssw24@users.noreply.github.com>
2024-10-26 13:20:07 +0200

    Update App.js x8

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ const VolumeMonitor = () => {
     return (
         <div className={`volume-monitor ${isLoud ? 'alert' : ''}`}>
             <h1>Volume Monitor 3 mit gpt</h1>
-            <p>Aktuelle Lautstärke: {displayedVolume} dB {decibels}</p>
+            <p>Aktuelle Lautstärke: {displayedVolume} dB</p>
             <p>{isLoud ? "Lautstärke überschritten!" : "Lautstärke im normalen Bereich"}</p>
 <p>Ausgabe: {ausgabe} - </p>
 
